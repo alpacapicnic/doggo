92295f946444640291f607eace3584d912e5964b
Tim McCoy <timmymccoy@gmail.com>
2018-03-19 22:48:43 -0700

    final commit

--- js/result.js
+++ js/result.js
@@ -16,10 +16,19 @@ var lab = new Dog ('breeds/labrador.jpg', 'Labrador Retriever', 'Labradors have 
 var doberman = new Dog ('breeds/doberman.jpg', 'Doberman Pinscher', 'The Doberman Pinscher has ranked amongst the most intelligent of dog breeds in experimental studies and expert evaluations.');
 var mastiff = new Dog ('breeds/bullmastiff.jpeg', 'Bullmastiff', 'Bullmastiffs are strong, powerful, but sensitive dogs.');
 
-
-
-
 var australian = new Dog ('breeds/austshepherd.jpeg', 'Australian Shepherd', 'Enjoys working, whether it is learning and practicing tricks, competing in dog agility, or engaging in any other physically and mentally involving activity.');
+var corgi = new Dog ('breeds/corgi.jpg', 'Corgi', 'They are of the type of herding dog referred to as "heelers", meaning that they would nip at the heels of the larger animals to keep them on the move.');
+var chow = new Dog ('breeds/chow.jpg', 'Chow Chow', 'This breed of dog has many strong loyal bonds with friends and family, but the Chow Chow dog is usually overly protective of one or two main family member.');
+var harrier = new Dog ('breeds/harrier.jpeg', 'Harrier', 'The Harrier is cheerful, sweet-tempered, tolerant of people, and it is excellent with children.');
+var basenji = new Dog ('breeds/basenji.jpg', 'Basenji', 'The Basenji tends to become emotionally attached to a single human.');
+var bulldog = new Dog ('breeds/bulldog.jpg', 'English Bulldog', 'Bulldogs are recognized as excellent family pets because of their tendency to form strong bonds with children.');
+
+var russell = new Dog ('breeds/jackrussell.jpg', 'Jack Russell', 'Jack Russells tend to be extremely intelligent, athletic, fearless, and vocal dogs.');
+var pom = new Dog ('breeds/pomeranian.jpg', 'Pomeranian', 'Pomeranians are typically friendly, playful and lively; but they can be aggressive with other dogs to try to prove themselves.');
+var shih = new Dog ('breeds/shih.jpg', 'Shih Tzu', 'Because of its friendly nature, the Shih Tzu tends to interact well with other dogs and with children and adults.');
+var dachshund = new Dog ('breeds/dachshund.jpg', 'Dachshund', 'Dachshunds are playful, but as hunting dogs can be quite stubborn.');
+var klee = new Dog ('breeds/klee.png', 'Alaskan Klee Kai', 'The Alaskan Klee Kai is a highly intelligent, playful, curious and high energy breed.');
+var frenchie = new Dog ('breeds/frenchie.jpg', 'French Bulldog', 'The French Bulldog, like many other companion dog breeds, requires close contact with humans.');
 
 if (selectedSize === 'large'){
     if (selectedFloor === 'shaggy'){
@@ -44,49 +53,41 @@ if (selectedSize === 'large'){
 else if (selectedSize === 'small'){
     if (selectedFloor === 'shaggy'){
         if (selectedCute === 'lion'){
-            //SetFinalVariables(australian);
-            var img = new Image();
-            var div = document.getElementById('resultImage');
-
-            img.onload = function() {
-            div.appendChild(img);   
-            };
-
-        img.src = australian.image;
+            SetFinalVariables(australian);
         }
         else if(selectedCute === 'fox'){
-            document.getElementById("dogChoice").innerHTML = "SMALL HAIRY FOX";
+            SetFinalVariables(corgi);
         }
-        else document.getElementById("dogChoice").innerHTML = "SMALL HAIRY BAT";
+        else SetFinalVariables(chow);
     }
     else if (true){
         if (selectedCute === 'lion'){
-            document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH LION";
+            SetFinalVariables(harrier);
         }
         else if(selectedCute === 'fox'){
-            document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH FOX";
+            SetFinalVariables(basenji);
         }
-        else document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH BAT";
+        else SetFinalVariables(bulldog);
     }
 }    
 else if (selectedSize === 'closet'){
     if (selectedFloor === 'shaggy'){
         if (selectedCute === 'lion'){
-            document.getElementById("dogChoice").innerHTML = "SMALL HAIRY LION";
+            SetFinalVariables(russell);
         }
         else if(selectedCute === 'fox'){
-            document.getElementById("dogChoice").innerHTML = "SMALL HAIRY FOX";
+            SetFinalVariables(pom);
         }
-        else document.getElementById("dogChoice").innerHTML = "SMALL HAIRY BAT";
+        else SetFinalVariables(shih);
     }
     else if (true){
         if (selectedCute === 'lion'){
-            document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH LION";
+            SetFinalVariables(dachshund);
         }
         else if(selectedCute === 'fox'){
-            document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH FOX";
+            SetFinalVariables(klee);
         }
-        else document.getElementById("dogChoice").innerHTML = "SMALL SMOOTH BAT";
+        else SetFinalVariables(frenchie);
     }
 }  
 
